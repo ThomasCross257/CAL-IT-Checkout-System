2b3ec7e1858d0a4908c2509ffac11feaeae904b2
Ph00sant <thomascross257@gmail.com>
2023-10-05 15:15:41 -0700

    Made further adjustments to the tag detector

--- tagDetector.py
+++ tagDetector.py
@@ -1,6 +1,4 @@
 import cv2
-from dotenv import load_dotenv
-import os
 from pytesseract import pytesseract
 # Path: tagDetector.py
 
@@ -12,21 +10,11 @@ def tagDetect(TessPath):
 
     while True:
         ret, frame = camera.read()
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        ret, buffer = cv2.imencode('.jpg', frame)
-        if not ret:
-            continue
-
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
         text = pytesseract.image_to_string(gray)
-
         if any(char.isdigit() for char in text):
-            return text
-
-    camera.release()
-    cv2.destroyAllWindows()
+            ret, buffer = cv2.imencode('.jpg', frame)
+            if not ret:
+                continue
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
